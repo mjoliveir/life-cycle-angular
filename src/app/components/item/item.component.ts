24d4d1610b81470002db6bea2b5a8d14f4138b48
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-19 14:55:28 -0300

    Implement item editing functionality and update component bindings

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, Output, SimpleChanges, EventEmitter } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges {
+  @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter()
+
 
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    console.log('OnInit')
+  }
 
+  ngOnChanges() {
+    console.log('OnChanges')
+  }
+
+  editarItem(){
+    this.emitindoItemParaEditar.emit(this.item);
+  }
 }
