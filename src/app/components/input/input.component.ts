2023c7141345a3d9a0dc88c2a4b660aadb030c33
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-19 14:55:21 -0300

    Implement item editing functionality and update input bindings

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,13 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Item } from 'src/app/interfaces/iItem';
+import { ListaDeCompraService } from 'src/app/service/lista-de-compra.service';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css']
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements OnInit, OnChanges {
+  @Input() itemQueVaiSerEditado!: Item;
+  editando = false;
+  textoBtn = 'Salvar Item'
 
-  constructor() { }
+  valorItem!: string 
+  constructor(private ListaService: ListaDeCompraService) { } //injeção dos metodos abaixo
 
   ngOnInit(): void { }
+  adicionarItem(){
+    this.ListaService.adicionarItemNaLista(this.valorItem) // chama o metodo adicionarItemNaLista (que vem de uma service e passa this.valorItem como parâmetro)
+    this.limparCampo() //limpa o input
+  }
+
+  limparCampo(){
+    this.valorItem = ''
+  }
+
+  editarItem(){
+    this.ListaService.editarItemDaLista(this.itemQueVaiSerEditado, this.valorItem)
+    this.limparCampo()
+    this.editando = false;
+    this.textoBtn = 'Salvar item'
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if(!changes['itemQueVaiSerEditado'].firstChange){
+      this.editando = true
+      this.textoBtn = 'Editar Item'
+      this.valorItem = this.itemQueVaiSerEditado?.nome
+    }
+  }
+
 }
