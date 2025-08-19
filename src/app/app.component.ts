27d243ab86214ade0eae486ab6bb3e2f72cbd982
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-08-19 14:55:13 -0300

    Implement item editing functionality and update template bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Item } from './interfaces/iItem';
+import { ListaDeCompraService } from './service/lista-de-compra.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app-lista-de-compras';
+  listaDeCompra!: Array<Item> //cria-se uma variavel para alocar um array do tipo item (ITEM É UMA INTERFACE E ESTÁ DISPONIVEL NA PASTA DE INTERFACES)
+  itemParaSerEditado! : Item;
+  
+  constructor(private listaService: //injeção do service de lista de compras
+     ListaDeCompraService) { }
 
-  constructor() { }
+     ngOnInit(): void {
+       this.listaDeCompra = this.listaService.getListaDeCompra(); // chama o metodo getListaDeCompras e atrela seu resultado a variavel que criamos (this.listaDeCompras)
+       console.log(this.listaDeCompra) //imprime no console os arrays da listaDeCompras
+     }
+
+     editarItem(item: Item){
+      this.itemParaSerEditado = item
+     }
 }
